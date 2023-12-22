dde28aea1925eb3b2ad510fee749de1e4e9e5527
Ritik-Agarwal50 <ritikagarwal570@gmail.com>
2023-12-22 18:19:44 +0530

    Hello_World

---
+++ hello_world.go
@@ -0,0 +1,14 @@
+/*
+Why to use package main?
+In Go, the package main declaration is used to indicate that the Go file is the entry point of the program. The main package is a special package, and it is required for creating executable programs in Go.
+
+Why ti Import fmt?
+The fmt package is one of the most commonly used packages in Go because it contains functions for printing to the console and scanning input from the user. The fmt package is part of the Go standard library, and it is always available for use in any Go application.
+*/
+
+package main
+
+import "fmt"
+func main(){
+	fmt.Println("Radhe Radhe")
+}
