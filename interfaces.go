0933db003b801ac1c0fb8825eb31367a60a78831
Ritik-Agarwal50 <ritikagarwal570@gmail.com>
2024-01-01 00:39:33 +0530

    interfaces

--- interfaces.go
+++ interfaces.go
@@ -1,4 +1,35 @@
- package main
-import "fmt"
+package main
 
-func main(){}
+import (
+	"fmt"
+	"math"
+)
+
+type shape interface{
+	area() float64
+}
+
+type rectangle struct{
+	width float64
+	height float64
+}
+
+type circle struct{
+	radius float64
+}
+
+func (r rectangle) area() float64{
+	return r.width * r.height
+}
+
+func (c circle) area() float64{
+	return math.Pi * c.radius * c.radius	
+}
+
+func main(){
+	r := rectangle{ 10,  20}
+	c := circle{69}
+
+	fmt.Println("Area of rectangle is: ", r.area())
+	fmt.Println("Area of circle is: ", c.area())
+}
